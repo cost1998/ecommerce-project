59e95f622e04311395a8e21bea876ec245399930
cost1998 <79930711+cost1998@users.noreply.github.com>
2021-05-17 15:14:29 +0300

    17/5 update

--- 02-backend/spring-boot-ecommerce/src/main/java/com/cost/ecommerce/dao/ProductCategoryRepository.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/cost/ecommerce/dao/ProductCategoryRepository.java
@@ -4,6 +4,6 @@ import com.cost.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
