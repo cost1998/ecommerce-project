8a7a86849450b527d6504e296a68e66e33ba1ee5
cost1998 <79930711+cost1998@users.noreply.github.com>
2021-06-06 11:53:28 +0300

    6/06 update

--- Frontend/angular-ecommerce/src/app/app.module.ts
+++ Frontend/angular-ecommerce/src/app/app.module.ts
@@ -13,15 +13,17 @@ import { ProductDetailsComponent } from './components/product-details/product-de
 
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
 
 const routes: Routes = [
-{path: 'products/:id', component: ProductDetailsComponent},
-{path: 'search/:keyword', component: ProductListComponent},
-{path: 'category/:id', component: ProductListComponent},
-{path: 'category', component: ProductListComponent},
-{path: 'products', component: ProductListComponent},
-{path: '', redirectTo: '/products', pathMatch: 'full'},
-{path: '**', redirectTo: '/products', pathMatch: 'full'},
+  {path: 'cart-details', component: CartDetailsComponent},
+  {path: 'products/:id', component: ProductDetailsComponent},
+  {path: 'search/:keyword', component: ProductListComponent},
+  {path: 'category/:id', component: ProductListComponent},
+  {path: 'category', component: ProductListComponent},
+  {path: 'products', component: ProductListComponent},
+  {path: '', redirectTo: '/products', pathMatch: 'full'},
+  {path: '**', redirectTo: '/products', pathMatch: 'full'},
 ];
 
 @NgModule({
@@ -31,7 +33,8 @@ const routes: Routes = [
     ProductCategoryMenuComponent,
     SearchComponent,
     ProductDetailsComponent,
-    CartStatusComponent
+    CartStatusComponent,
+    CartDetailsComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
